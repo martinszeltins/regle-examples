e4987150694b4930e98db6804846b23b6f1e0fcb
Victor Garcia <victor.garcia@malt.com>
2024-12-14 14:38:58 +0100

    remove cors

--- examples/simple-example/vite.config.js
+++ examples/simple-example/vite.config.js
@@ -3,19 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    {
-      name: 'add-cors',
-
-      configureServer(server) {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-          res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
-          next();
-        });
-      },
-    },
-  ],
+  plugins: [vue()],
 });
