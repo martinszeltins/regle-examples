1e75a26133236254bbdd66d3515e60eb8b901773
Victor Garcia <victor.garcia@malt.com>
2024-12-14 14:05:46 +0100

    test cors

--- examples/simple-example/vite.config.js
+++ examples/simple-example/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    {
+      name: 'add-cors',
+
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+          next();
+        });
+      },
+    },
+  ],
+});
